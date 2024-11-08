5c4aca8dc9c08cad6e9360f5007fa29a4e26c421
MartaCanino <martacaninoromero@gmail.com>
2024-11-08 11:21:46 +0100

    Personalizar nombre contenedores container-fluid para evitar problemas en css

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,11 +1,13 @@
 document.getElementById("menu-toggle").addEventListener("click", function() {
     let sidebar = document.getElementById("sidebar-wrapper");
     let content = document.getElementById("page-content-wrapper");
-    
-    if (sidebar.style.width === "250px") {
+
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
         sidebar.style.width = "0";
         content.style.paddingLeft = "0";
     } else {
+        sidebar.style.display = "block";
         sidebar.style.width = "250px";
         content.style.paddingLeft = "250px";
     }
