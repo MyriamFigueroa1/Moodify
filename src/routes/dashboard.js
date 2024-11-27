e387aa048d13f1307e3cdfb74cd5878ab93550b9
karencl <69648059+karencl@users.noreply.github.com>
2024-11-27 21:57:58 +0100

    Visualización y edición del perfil

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('dashboard');
+router.get('/', (req, res) => {
+  res.render('dashboard', { user: req.session.user });
 });
 
 module.exports = router;
