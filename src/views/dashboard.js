a0c26dd7ded8ee930237e9d7b62727cd9253d48d
karencl <69648059+karencl@users.noreply.github.com>
2024-11-27 21:58:55 +0100

    Delete src/views/dashboard.js

--- src/views/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('dashboard', { user: req.session.user });
-});
-
-module.exports = router;
