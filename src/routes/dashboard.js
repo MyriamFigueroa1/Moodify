789730356be138a8290fafd6cb329866f1902f81
MartaCanino <martacaninoromero@gmail.com>
2024-11-30 19:34:59 +0100

    publicaciones de texto funcionales (falta nombre usuario)

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,41 +3,39 @@ const router = express.Router();
 const { getDb } = require('../db/conn');
 
 
+// Ruta para renderizar el dashboard
 router.get('/', async (req, res) => {
-  try{
+  try {
     const db = getDb();
-    const posts = await db.collection('posts').find().toArray();
-    console.log(posts);
-    
+    const posts = await db.collection('posts').find().sort({ timestamp: -1 }).toArray();
+
     res.render('dashboard', {
       user: req.session.user,
       posts: posts
     });
-  } catch (error){
+  } catch (error) {
     console.error('Error al obtener los posts:', error);
     res.status(500).send('Error al obtener los posts');
   }
 });
 
-router.post('/createpost', async (req, res)=> {
-  const {emoji, songs} = req.body;
-  const userId = req.session.user._id;
-
-  try{
+// Ruta para agregar un nuevo post
+router.post('/add', async (req, res) => {
+  try {
     const db = getDb();
+
     const newPost = {
-      userId,
-      userName: req.session.user.userName,
-      emoji,
-      content: songs
+      userID: req.session.user._id,
+      userName: req.session.user.name,
+      content: req.body.content,
+      timestamp: new Date()
     };
 
-    await db.collection('posts').insertOne(newPost);
-
-    res.json({ success: true });
-  } catch (error){
-    console.error('Error al guardar la publicación:', error);
-    res.status(500).json({ success: false, error: 'Error al guardar la publicación' });
+    await db.collection('posts').insertOne(newPost); // Insertar el post en la base de datos
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.error('Error al agregar un nuevo post:', error);
+    res.status(500).send('Error al agregar un nuevo post');
   }
 });
 
